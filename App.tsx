a64704b385d2298b0837e149b2ee43e789bc6431
vaheghratyan <vaheghratyan@gmail.com>
2023-11-14 15:32:25 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RootNavigator } from './navigations/RootNavigator';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
+}
